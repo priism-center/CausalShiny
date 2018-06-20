1ec1fc08c2615ba73b39435ab0e88de6bb49df3a
George Yang <ly593@nyu.edu>
2018-06-20 01:25:26 -0400

    The Initial Stucture 6/20

--- CausInferShiny.R
+++ CausInferShiny.R
@@ -1,85 +1,189 @@
-# SHINY TEMPLATE
-
-
-
-
-
-
-
-
-
-
 library(shiny)
+library(bartCause)
 
-# Define UI for application that draws a histogram
+########################################
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Central Limit Theorem"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        selectInput("distribution", "Distribution Type:", c(Normal = "normal", Beta = "beta", Uniform = "uniform")), 
-        conditionalPanel(
-        condition = "input.distribution == 'normal'",
-          sliderInput("mean", "Mean:", min = 1720, max = 1750, value = 1729),
-          sliderInput("sd", "Standard Deviation:", min = 1, max = 20, value = 10),
-          sliderInput("nsample", "Number of Samples:", min = 1000, max = 10000, value = 2000),
-          sliderInput("sampleSize", "Sample size:", min = 50, max = 100, value = 100)),
-        
-        conditionalPanel(
-        condition = "input.distribution == 'beta'",
-          sliderInput("alpha", "Alpha:", min = 1, max = 5, value = 1),
-          sliderInput("beta", "Beta:", min = 1, max = 5, value = 1)),
-        
-        conditionalPanel(
-        condition = "input.distribution == 'uniform'",
-          sliderInput("min", "Min:", min = 0, max = 5, value = 0),
-          sliderInput("interval", "Interval:", min = 1, max = 5, value = 1))
-      ),
-          
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("normdist"), 
-         plotOutput("normhist")
-      )
-   )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-   
-   output$normdist <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x <- matrix(rep(0, input$sampleSize * input$nsample), nrow = input$nsample, ncol = input$sampleSize)
-      for (i in 1:input$nsample) {
-        if (input$distribution == "normal") {
-      x[i, ] <- rnorm(input$sampleSize, input$mean, input$sd)
-        }
-        else if (input$distribution == "beta") {
-      x[i, ] <- rbeta(input$sampleSize, input$alpha, input$beta)
-        }
-        else if (input$distribution == "uniform") {
-      x[i, ] <- runif(input$sampleSize, input$min, input$min + input$interval)    
-        }
-      }
-      y <- apply(x, 1, mean)
-      # draw the histogram with the specified number of bins
-      plot(density(y), main = "Distribution")
-   })
   
-   output$normhist <- renderPlot({
-     # generate bins based on input$bins from ui.R
-     x <- matrix(rep(0, input$sampleSize * input$nsample), nrow = input$nsample, ncol = input$sampleSize)
-     for (i in 1:input$nsample) {
-       x[i, ] <- rnorm(input$sampleSize, input$mean, input$sd)
-     }
-     y <- apply(x, 1, mean)
-     hist(y)
-   })
+  # Navigating Tabs
+  navbarPage("Causal Inference",
+             
+             # Tab Panel 1
+             tabPanel("Panel1 - Introduction", 
+                      
+                      # Sidebar Layout
+                      sidebarLayout(
+                        
+                        # Sidebar Panel
+                        sidebarPanel(
+                          
+                          # Text
+                          h3("Introduction"),
+                          p("This RShiny application aims to simplify the process of conducting Causal Inference.")
+                        ),
+                        
+                        # Main Panel
+                        mainPanel(
+                          
+                          # Text
+                          h1("Instructions", align = "center"),
+                          p("Bypassing coding, this application allows the users to simply select different
+                            options through the interface to get live results."),
+                          br(),
+                          h4("Step 1."),
+                          p("Trim dataset so it only includes confounders (X), treatment (Y), 
+                            response (Z), and ID column (optional)."),
+                          h4("Step 2."),
+                          p("Convert and save dataset into .csv file."),
+                          h4("Step 3."),
+                          p("Click Upload panel on top of the page and choose the file path to upload dataset. 
+                            Specify the column number of ID column (if applicable), treatment column (Y), 
+                            and response column (Z)."),
+                          h4("Step 4."),
+                          p("Use Option panel to select method of analysis."),
+                          h3("R Packages Used"),
+                          p(a("BARTCause", href = "https://github.com/vdorie/bartCause"), " and ",
+                            a("treatSens", href = "https://cran.r-project.org/web/packages/treatSens/index.html"))
+                          
+                          
+                          )
+                          )
+                      ),
+             
+             # Tab Panel 2
+             tabPanel("Panel2 - Upload", 
+                      
+                      # Sidebar Layout
+                      sidebarLayout(
+                        
+                        # Sidebar Panel
+                        sidebarPanel(
+                          
+                          # Input: file
+                          fileInput("file", "Choose CSV File",
+                                    multiple = FALSE,
+                                    accept = c("text/csv",
+                                               "text/comma-separated-values,text/plain",
+                                               ".csv")),
+                          
+                          # Horizontal line
+                          tags$hr(),
+                          
+                          # Input: header
+                          checkboxInput("header", "Header", TRUE),
+                          
+                          # Input: ID Column
+                          checkboxInput("id", "Subject ID column?", FALSE), 
+                          
+                          conditionalPanel(
+                            condition = "input.id",
+                            numericInput("idcol", "Subject ID Column Number", value = 1)
+                          ),
+                          
+                          # Horizontal line
+                          tags$hr(),
+                          
+                          # Input: Y Column and Z column
+                          numericInput("zcol", "Treatment (Y) Column Number", value = 2),
+                          numericInput("ycol", "Response (Z) Column Number", value = 3)
+                        ),
+                        
+                        # Main Panel
+                        mainPanel(
+                          
+                          # Output: Data file ----
+                          tableOutput("uploads")
+                        )
+                      )
+             ),
+             
+             # Tab Panel 3
+             tabPanel("Panel3 - Options", 
+                      
+                      # Sidebar layout with input and output definitions ----
+                      sidebarLayout(
+                        
+                        # Sidebar Panel
+                        sidebarPanel(
+                          
+                          # Input: Select Estimand
+                          radioButtons("estimand", "Select Estimand",
+                                       choices = c(ATE = "ATE",
+                                                   ATT = "ATT",
+                                                   ATC = "ATC"),
+                                       selected = "ATE"),
+                          
+                          tags$hr(),
+                          
+                          # Input: Survey Weights
+                          checkboxInput("sweight", "Survey Weights", FALSE),
+                          
+                          # Input: Add Propensity Score
+                          checkboxInput("pscore", "Add Propensity Score", FALSE),
+                          
+                          conditionalPanel(
+                            condition = "input.pscore",
+                            radioButtons("pscoreas", "Propensity Score",
+                                         choices = c("As Covariate" = ",",
+                                                     "As Weight" = ";"),
+                                         selected = ","))
+                        ),
+                        
+                        
+                        # Main Panel
+                        mainPanel(
+                          
+                          # Output: plots
+                          plotOutput("postplot")
+                        )
+                      )
+             )
+             
+  )
+  )
+
+
+
+
+#########################################
+server <- function(input, output) {
+  
+  output$uploads <- renderTable({
+    
+    req(input$file)
+    
+    tryCatch(
+      {
+        df <- read.csv(input$file$datapath,
+                       header = input$header)
+      },
+      error = function(e) {
+        stop(safeError(e))
+      }
+    )
+    return(head(df))
+    
+  })
+  
+  output$postplot <- renderPlot({
+    
+    tryCatch(
+      {
+        fit1 <- bartc(response = df[, ycol], treatment = df[, zcol], 
+                      confounders = df[, c(-ycol, -zcol)], data = df, 
+                      method.rsp = "bart", method.trt = "none", 
+                      commonSup.rule = "none", commonSup.cut = NA_real_)
+        plot_sigma(fit1)
+        plot_support(fit1)
+        plot_est(fit1)
+      },
+      error = function(e) {
+        stop(safeError(e))
+      }
+    )
+    
+  })
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
