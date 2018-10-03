b4d1d89efbf56358284facff42b0f91116aa11ad
georgemag121 <ly593@nyu.edu>
2018-10-03 13:29:36 -0400

    inital import

---
+++ functions.R
@@ -0,0 +1,38 @@
+# functions
+
+idcheck <- function(matr) {
+  idlist <- c()
+  
+  for (i in 1:ncol(matr)) {
+    if (length(unique(matr[, i])) == nrow(matr)) {
+      idlist <- c(idlist, names(matr)[i])
+    }
+  }
+  
+  invisible(idlist)
+}
+
+# dataset validation
+datacheck <- function(data, confound, trt, resp) {
+  if (data == "") {
+    "Please Upload a dataset"
+  }
+  
+  else {
+    
+    if (confound == "" || trt == "" || resp == "") {
+      "Please identify X, Y, Z"
+    }
+    
+    else {
+      
+      if (length(unique(data[ ,which(names(data) == trt)])) > 2) {
+        "Please check treatment variable selection, and/or missing values"
+      }
+      
+      else {
+        NULL
+      }
+    }
+  }
+}
