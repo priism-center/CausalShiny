0856d980458a3d3ab3d9797fa8491b19715e0f6b
georgemag121 <ly593@nyu.edu>
2018-10-10 15:28:40 -0400

    update

--- texts.r
+++ texts.r
@@ -1,3 +1,5 @@
 text1 <- "Average Treatment Effect (ATE), Average Treatment Effect on the Control (ATC), Average Treatment Effect on the Treated (ATT)"
 
 text2 <- "Propensity score measures the probability of receiving treatment"
+
+text3 <- "Targeted Maximum likelihood Estimation: Mark Van Der Laan, Daniel Rubin"
