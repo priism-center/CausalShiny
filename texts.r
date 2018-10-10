0f53165801dac397a5e2cb4356b3401dd2903a90
georgemag121 <ly593@nyu.edu>
2018-10-10 14:01:42 -0400

    initial import

---
+++ texts.r
@@ -0,0 +1,3 @@
+text1 <- "Average Treatment Effect (ATE), Average Treatment Effect on the Control (ATC), Average Treatment Effect on the Treated (ATT)"
+
+text2 <- "Propensity score measures the probability of receiving treatment"
